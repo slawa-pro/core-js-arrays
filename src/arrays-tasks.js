97a50a9893b38eaec526060d134123f79611ef51
Vyacheslav <va.povhleb@gmail.com>
2024-12-15 16:22:35 +0300

    init: start task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,13 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  const result = [];
+  for (let i = start; i <= end; ) {
+    result.push(i);
+    i += 1;
+  }
+  return result;
 }
 
 /**
