682e18b1610948a23e7a49a1b4eea35f91af312d
Vyacheslav <va.povhleb@gmail.com>
2025-01-12 11:39:41 +0300

    refactor: fix error 169220804

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -559,10 +559,8 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(arr, n) {
-  const len = arr.length;
-  n = ((n % len) + len) % len;
-  return [...arr.slice(-n), ...arr.slice(0, len - n)];
+function shiftArray(/* arr, n */) {
+  throw new Error('Not implemented');
 }
 
 /**
